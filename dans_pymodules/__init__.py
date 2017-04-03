56d5675d5671bb320711323efe590089541158fc
Daniel <winklehn@mit.edu>
2017-04-03 15:54:41 -0400

    added MPLCanvasWrapper

--- dans_pymodules/__init__.py
+++ dans_pymodules/__init__.py
@@ -2,3 +2,6 @@ from particles import *
 from mycolors import *
 from filedialog import *
 from pylatex import *
+from particle_distribution import *
+from label_combo import *
+from mpl_canvas_wrapper import *
