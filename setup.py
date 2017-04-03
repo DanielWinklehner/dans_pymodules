626001039fb30818e21167572b1c0f5ad087e655
Daniel <winklehn@mit.edu>
2017-04-03 16:35:26 -0400

    minor changes to vector

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dans_pymodules',
-      version='2.5',
+      version='2.6',
       description='Useful little modules that I likely need in more than one application',
       url='https://github.com/DanielWinklehner/dans_pymodules',
       author='Daniel Winklehner, Philip Weigel',
