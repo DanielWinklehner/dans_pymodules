9dd811225e8379a9dc14300e816edea3656ea4e6
pweigel <plw38@drexel.edu>
2017-05-02 18:35:33 -0400

    Added Class Structure

--- dans_pymodules/ascii2h5block.py
+++ dans_pymodules/ascii2h5block.py
@@ -1,40 +1,74 @@
 import h5py
 import numpy as np
 
-spacing = np.array([1.0, 1.0, 1.0])
-rmin = np.array([-80.0, -80.0, -100.0])
-rmax = np.array([80.0, 80.0, 50.0])
 
-size = np.array((rmax - rmin) / spacing + 1, int)
-print(size)
+class Ascii2H5(object):
+    def __init__(self, filename, spacing, r_min, r_max, _output_dir=None):
+        self.spacing = spacing
+        self.r_min = r_min
+        self.r_max = r_max
 
-data = {"ex": np.zeros(size),
-        "ey": np.zeros(size),
-        "ez": np.zeros(size),
-        "hx": np.zeros(size),
-        "hy": np.zeros(size),
-        "hz": np.zeros(size)
-        }
+        # TODO: Add path to filename
+        # Create a new h5 file
 
-h5_file = h5py.File("C:/Users/Daniel/Desktop/Test.h5", )
+        self.h5_file = h5py.File(filename, )
+        # h5_file = h5py.File("C:/Users/Daniel/Desktop/Test.h5", )
 
-step0 = h5_file.create_group("Step#0")
-block = step0.create_group("Block")
+        # Calculates the size of data arrays
+        _size = np.array((r_max - r_min) / spacing + 1, int)
 
-efield = block.create_group("Efield")
-efield_x = efield.create_dataset("0", data=data["ex"])
-efield_y = efield.create_dataset("1", data=data["ey"])
-efield_z = efield.create_dataset("2", data=data["ez"])
+        # Data Format:
+        # Dictionary with keys "ex", "ey", "ez", "hx", "hy", and "hz", which correspond to the vector components
+        # of the electric field and the H field.
+        self.data = {"ex": np.zeros(_size),
+                     "ey": np.zeros(_size),
+                     "ez": np.zeros(_size),
+                     "hx": np.zeros(_size),
+                     "hy": np.zeros(_size),
+                     "hz": np.zeros(_size)}
 
-efield.attrs.__setitem__("__Spacing__", spacing)
-efield.attrs.__setitem__("__Origin__", rmin)
+    def set_data(self, data):
+        self.data = data
 
-hfield = block.create_group("Hfield")
-hfield_x = hfield.create_dataset("0", data=data["hx"])
-hfield_y = hfield.create_dataset("1", data=data["hy"])
-hfield_z = hfield.create_dataset("2", data=data["hz"])
+    def generate(self):
 
-hfield.attrs.__setitem__("__Spacing__", spacing)
-hfield.attrs.__setitem__("__Origin__", rmin)
+        # Create the zeroth step and the Block inside of it
+        step0 = self.h5_file.create_group("Step#0")
+        block = step0.create_group("Block")
 
-h5_file.close()
+        # Create the E Field group
+        e_field = block.create_group("Efield")
+
+        # Store the x, y, and z data for the E Field
+        e_field.create_dataset("0", data=self.data["ex"])
+        e_field.create_dataset("1", data=self.data["ey"])
+        e_field.create_dataset("2", data=self.data["ez"])
+
+        # Set the spacing and origin attributes for the E Field group
+        e_field.attrs.__setitem__("__Spacing__", self.spacing)
+        e_field.attrs.__setitem__("__Origin__", self.r_min)
+
+        # Create the H Field group
+        h_field = block.create_group("Hfield")
+
+        # Store the x, y, and z data points for the H Fiend
+        h_field.create_dataset("0", data=self.data["hx"])
+        h_field.create_dataset("1", data=self.data["hy"])
+        h_field.create_dataset("2", data=self.data["hz"])
+
+        # Set the spacing and origin attributes for the H Field group
+        h_field.attrs.__setitem__("__Spacing__", self.spacing)
+        h_field.attrs.__setitem__("__Origin__", self.r_min)
+
+        # Close the file
+        self.h5_file.close()
+
+if __name__ == '__main__':
+    spacing = np.array([1.0, 1.0, 1.0])
+    r_min = np.array([-80.0, -80.0, -100.0])
+    r_max = np.array([80.0, 80.0, 50.0])
+    filename='my_h5.h5'
+
+    my_h5 = Ascii2H5(filename=filename, spacing=spacing, r_min=r_min, r_max=r_max)
+    my_h5.generate()
+
