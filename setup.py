5c573c0e8e7b45fd085e5664b11c002f8217ff1d
Daniel Winklehner <winklehn@mit.edu>
2017-05-11 14:23:11 -0400

    included non-python files in setup.py script. -DW

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='dans_pymodules',
-      version='3.0',
+      version='3.1',
       description='Useful little modules that I likely need in more than one application',
       url='https://github.com/DanielWinklehner/dans_pymodules',
       author='Daniel Winklehner, Philip Weigel',
       author_email='winklehn@mit.edu',
       license='MIT',
       packages=['dans_pymodules'],
+      package_data={'': ['PlotSettingsDialog.glade', 'header.tex', 'fishfinder.png', 'vitruvian.jpg']},
+      include_package_data=True,
       zip_safe=False)
